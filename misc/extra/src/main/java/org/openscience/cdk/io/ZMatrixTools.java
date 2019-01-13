9cbe44e4d5f1f301a5b52dbbe3ba3d794b0e5cd2
John Todd <jtodd@pillartechnology.com>
2019-01-12 22:23:28 -0500

    unsplit org.openscience.cdk.geometry from cdk.io by moving a class and its test

--- misc/extra/src/main/java/org/openscience/cdk/io/ZMatrixTools.java
+++ misc/extra/src/main/java/org/openscience/cdk/io/ZMatrixTools.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *  */
-package org.openscience.cdk.geometry;
+package org.openscience.cdk.io;
 
 import javax.vecmath.AxisAngle4f;
 import javax.vecmath.Matrix3f;
