5686733f03263ea9b2dbb9d3fe07a374d4cb6088
John Todd <jtodd@pillartechnology.com>
2019-01-13 23:22:44 -0500

    unsplit org.openscience.cdk.fingerprint, merge cdk-similarity into cdk-fingerprint

--- descriptor/fingerprint/src/main/java/org/openscience/cdk/fingerprint/LingoFingerprinter.java
+++ descriptor/fingerprint/src/main/java/org/openscience/cdk/fingerprint/LingoFingerprinter.java
@@ -21,7 +21,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package org.openscience.cdk.smilesfingerprint;
+package org.openscience.cdk.fingerprint;
 
 import org.openscience.cdk.aromaticity.Aromaticity;
 import org.openscience.cdk.aromaticity.ElectronDonation;
--- descriptor/fingerprint/src/test/java/org/openscience/cdk/fingerprint/LingoFingerprinterTest.java
+++ descriptor/fingerprint/src/test/java/org/openscience/cdk/fingerprint/LingoFingerprinterTest.java
@@ -21,7 +21,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package org.openscience.cdk.smilesfingerprint;
+package org.openscience.cdk.fingerprint;
 
 import org.junit.Assert;
 import org.junit.Test;
